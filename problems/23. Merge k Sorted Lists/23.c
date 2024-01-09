2952bd29059a3c8f59fcf5993ac3aa1bc8e66095
TsukiCU <hs3475@columbia.edu>
2024-01-09 02:36:23 -0500

    Jan 8, 2024

---
+++ problems/23. Merge k Sorted Lists/23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+
+}
+
+int main()
+{
+
+}
