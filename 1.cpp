4e266e1219ad3c9c583013046f1ac4ea62fe59e4
TsukiCU <hs3475@columbia.edu>
2024-01-31 18:29:49 -0500

    Jan 31, 2024

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <string_view>
+#include <1.hh>
+
+namespace A
+{
+    namespace D
+    {
+        int number_2 = 1;
+    }
+}
+
+int main()
+{
+    std::cout
+}
---
+++ 1.hh
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdint.h>
+#include<assert.h>
+#include<vector>
+
+namespace A
+{
+    namespace b
+    {
+        namespace C
+        {
+            int number = 1;
+        }
+    }
+}
