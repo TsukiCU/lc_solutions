1d6ddc41d493e3ec4e23d2443c927f3575610953
TsukiCU <hs3475@columbia.edu>
2024-02-17 12:40:51 -0500

    Feb 17, 2024

--- Problem/22. Generate Parentheses/22.cpp
+++ Problem/22. Generate Parentheses/22.cpp
@@ -1,9 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
-
 using namespace std;
-
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
@@ -11,7 +9,6 @@ public:
             return {""};
         }
         vector<string> res;
-
         for (int p = 0; p < n; ++p) {
             for (const auto& p_ : generateParenthesis(p)) {
                 for (const auto& q_ : generateParenthesis(n - 1 - p)) {
@@ -19,11 +16,9 @@ public:
                 }
             }
         }
-
         return res;
     }
 };
-
 int main()
 {
     Solution s;
